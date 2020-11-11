8041487969d2ea09acfd4164dd4d1041f135714f
dujiahao <610153956@qq.com>
2020-11-12 01:39:43 +0800

    选择符文页

--- src/components/ShowDetail.vue
+++ src/components/ShowDetail.vue
@@ -1,15 +1,54 @@
 <template id='ShowDetail'>
     <div class="row-content background-img">
-        <el-row>showDetail{{championId}}</el-row>
+        <el-row>{{championDetail.name}} - {{championDetail.title}}</el-row>
+        <el-row>
+            <el-col :span="12">
+                <div @click="selectType('opgg')">opgg</div>
+            </el-col>
+            <el-col :span="12">
+                <div @click="selectType('qq')">qq</div>
+            </el-col>
+        </el-row>
 
-        <div v-for="(perk ,index) in qqPerks" :key="index">
-            <div @click="apply(perk)" @mouseenter="showPreview(perk)" @mouseleave="hidePreview">
-                {{perk.alias}}
-                {{perk.position}}
-                {{perk.winRate}}
-                {{perk.pickCount}}
+        <el-row class="title-row">
+            <el-col :span="3"></el-col>
+            <el-col :span="6">位置</el-col>
+            <el-col :span="6">胜率</el-col>
+            <el-col :span="6">选择次数</el-col>
+        </el-row>
+        <div v-show="type==='qq'">
+            <div v-for="(perk ,index) in qqPerks" :key="index">
+                <el-row class="perk-row" type="flex" @mouseenter="showPreview(perk)" @mouseleave="hidePreview">
+                    <el-col :span="3" class="perk-img-content"><img class="perk-img"
+                                                                    :src="'/runes/'+perk.selectedPerkIds[0]+'.png'">
+                    </el-col>
+                    <el-col :span="6">{{perk.position}}</el-col>
+                    <el-col :span="6">{{perk.winRate}}%</el-col>
+                    <el-col :span="6">{{perk.pickCount}}</el-col>
+                    <el-col :span="3">
+                        <div @click="apply(perk)">应用</div>
+                    </el-col>
+                </el-row>
             </div>
         </div>
+
+
+        <div v-show="type==='opgg'">
+            <div v-for="(perk ,index) in opggPerks" :key="index">
+                <el-row class="perk-row" type="flex" @mouseenter="showPreview(perk)" @mouseleave="hidePreview">
+                    <el-col :span="3" class="perk-img-content"><img class="perk-img"
+                                                                    :src="'/runes/'+perk.selectedPerkIds[0]+'.png'">
+                    </el-col>
+                    <el-col :span="6">{{perk.position}}</el-col>
+                    <el-col :span="6">{{perk.winRate}}%</el-col>
+                    <el-col :span="6">{{perk.pickCount}}</el-col>
+                    <el-col :span="3">
+                        <div @click="apply(perk)">应用</div>
+                    </el-col>
+                </el-row>
+            </div>
+        </div>
+
 
     </div>
 </template>
@@ -28,11 +67,10 @@
     const ipcRenderer = window.electron.ipcRenderer;
     export default {
         name: "ShowDetail",
-        props: {
-            msg: []
-        },
+        props: {},
         data: function () {
             return {
+                type: "opgg",
                 championId: "",
                 championDetail: {},
                 championMap: [],
@@ -43,6 +81,9 @@
             };
         },
         methods: {
+            selectType(type) {
+                this.type = type;
+            },
             init: function () {
                 const lolVer = config.get(`lolVer`);
                 getChampions(lolVer).then((championList) => {
@@ -125,10 +166,33 @@
         },
         mounted: function () {
             console.log("ShowDetail");
+            config.set(`test`, `zh-CN`);
+            console.log(config.get(`test`));
+            const lolDir = config.get(`lolDir`);
             this.init();
         }
     }
 </script>
 <style>
 
+    .title-row {
+        height: 30px;
+        line-height: 30px;
+    }
+
+    .perk-row {
+        height: 50px;
+        align-items: center;
+        line-height: 50px;
+    }
+
+    .perk-img {
+        height: 30px;
+    }
+
+    .perk-img-content {
+        justify-content: center;
+        align-items: center;
+        display: flex;
+    }
 </style>
